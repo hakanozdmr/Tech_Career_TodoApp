fea9d8a0193d0c42ac482d24c05d95470612768f
hakanozdmr <ozdemirhakan346@gmail.com>
2023-09-08 16:23:36 +0300

    Created Todo.jsx

---
+++ todo-fe/src/components/Todo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TodoInput from './Todo/TodoInput'
+import TodoList from './Todo/TodoList'
+
+export default function Todo() {
+  return (
+    <div >
+        <TodoInput></TodoInput>
+        <TodoList></TodoList>
+    </div>
+  )
+}
