7d3801ba627e1320b22e75ec629a094ae4100bd6
hakanozdmr <ozdemirhakan346@gmail.com>
2023-09-08 16:23:56 +0300

    Updated TodoService.js

--- todo-fe/src/services/TodoService.js
+++ todo-fe/src/services/TodoService.js
@@ -1,14 +1,32 @@
 import axios from "axios";
-const API_URL = 'http://localhost:5000/api/todo';
+const API_URL = 'http://localhost:8080/v1/todo';
 
 export default class TodoService  {
-   getUsers(){
-    return axios.get(API_URL )
+  getAllTodos(){
+    return axios.get(API_URL)
   }
-  getTodos(){
-    return axios.get("http://localhost:5000/api/todo")
+  getAllTodosIsDoneTrue(){
+    return axios.get(API_URL+"/getAllTodosIsDoneTrue")
+  }
+  getAllTodosIsDoneFalse(){
+    return axios.get(API_URL+"/getAllTodosIsDoneFalse")
   }
   addTodo(todo){
-    return axios.post("http://localhost:5000/api/todo/",todo)
+    return axios.post(API_URL,todo)
+  }
+  deleteTodosIsDoneTrue(){
+    return axios.delete(API_URL+"/delete/deleteTodoIsDoneTrue")
+  }
+  deleteTodosAll(){
+    return axios.delete(API_URL+"/delete")
+  }
+  deleteTodoById(id){
+    return axios.delete(API_URL+`/delete/${id}`)
+  }
+  updateTodoContent(id,content){
+    return axios.put(API_URL+`/update/${id}/updateContent/?content=${content}`)
+  }
+  updateTodoIsDone(id){
+    return axios.put(API_URL+`/update/updateTodoIsDone/${id}`)
   }
 }
